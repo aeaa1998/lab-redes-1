7c832d0f4b46e4ea1134487ef6b19f7e9c24a8c7
Augusto Alonso <augustoalonso98@gmail.com>
2021-08-03 23:55:40 -0600

    maldita sea

--- hamming.py
+++ hamming.py
@@ -1,3 +1,4 @@
+from noice import generate_noise
 from bitarray import bitarray
 from constants import *
 from functools import reduce
@@ -44,6 +45,7 @@ def set_parity_values(bitstring_represntation, number_of_parity_checks):
                 reached = counter == index_to_check
 
         # Set parity index value
+        # print(sum_of_parity)
         if sum_of_parity % 2 == 0:
             parity_values[index_to_check] = "0"
         else:
@@ -52,6 +54,7 @@ def set_parity_values(bitstring_represntation, number_of_parity_checks):
 
     # Set values
     bitstring_represntation_list = list(bitstring_represntation)
+
     for index in parity_values:
         bitstring_represntation_list[index] = parity_values[index]
     if sum([int(i) for i in bitstring_represntation_list if int(i)]) % 2 != 0:
@@ -63,6 +66,7 @@ def normalize_text_with_parity_bits(data):
     _data = data+""
     data_size = len(_data)
     _mod = 16
+
     # bytes_to_add = (_mod - (data_size % _mod))
     bytes_to_add = get_mod_diff(_mod, data_size)
     # print("Agregaremos", bytes_to_add)
@@ -72,18 +76,19 @@ def normalize_text_with_parity_bits(data):
     else:
         size = bytes_to_add + data_size
 
-    number_of_parity_checks = int(math.log2(size))
-    # print("Parity", number_of_parity_checks)
+    number_of_parity_checks = round(math.log2(size))
+    # print("Parity", number_of_/parity_checks)
     _data = '0'*(bytes_to_add) + _data
 
     output = ""
+
     parity_dictionary = {"positions": {}, "add": bytes_to_add}
     for index in range(len(_data)):
         if index == 0:
             parity_dictionary["positions"][0] = _data[index]
             output += "0"
         elif math.log2(index).is_integer():
-            pos = 2**int(math.log2(index))
+            pos = 2**round(math.log2(index))
             parity_dictionary["positions"][pos] = _data[pos]
             output += "0"
         else:
@@ -108,19 +113,23 @@ def correct_dummies(data):
 
 
 def hamming_correct(data):
-    index_error = reduce(op.xor, [i for i, v in enumerate(data) if int(v)])
-    # print("Pos error", index_error)
+    index_error = reduce(lambda x, y: x ^ y, [i
+                         for (i, v) in enumerate(data) if int(v)])
     if index_error != 0:
+
         error_position = index_error
         data = list(data)
         data[error_position] = "0" if data[error_position] == "1" else "1"
         data = ''.join(data)
+
     return data
 
 
 def hamming_original(corrected, dictionary):
     corrected = list(corrected)
+
     for index in dictionary["positions"]:
+
         corrected[int(index)] = dictionary["positions"][index]
     return ''.join(corrected)[int(dictionary["add"]):]
 
@@ -130,21 +139,41 @@ def hamming_original(corrected, dictionary):
 # print(math.sqrt(res))
 # print(get01('a'))
 # print(16-(32 % 16))
-# dictionary, normalized = normalize_text_with_parity_bits("00110001")
+# bit_instance = bitarray()
+# bit_instance.frombytes(
+#     "block pasamos un monton de tiempo pero aunquesea encontramos el maldito error block pasamos un monton de tiempo pero aunquesea encontramos el maldito error".encode(ENCODING_FORMAT))
+# data = bit_instance.to01()
+
+# dictionary, normalized = normalize_text_with_parity_bits(bit_instance.to01())
 # out = ""
+
 # for i, v in enumerate(normalized):
 #     out += v
-#     if (i+1) % 4 == 0 and i != 0:
+#     if (i+1) % 16 == 0 and i != 0:
 #         out += "\n"
 
-# "0010100010110001"
-# corrupted = "0010100010111001"
+# # print(out)
+
+# # Real
+# # "1111000111110101011001010010000011110000011000010111001101101111101000000110001101101111011011110110111101101111"
+# # Error
+# # "0111000111/010101011001010010000011110000011000010111001101101111001000000100001101101111011011110110111101101111"
+# # "0111000111010101011001010010000011110000011000010111001101101111001000000100001101101111011011110110111101101111"
+# # Corrected
+# # ""
+# corrupted = generate_noise(normalized, 100)
+# # print("Corrupted: ", corrupted)
+# # print("Real: ", normalized)
+# # print("Are equal? ", normalized == corrupted)
+# # number_of_parity_checks = int(math.log2(len(corrupted)))
+# # print(set_parity_values(corrupted, number_of_parity_checks))
 
 # corrected = hamming_correct(corrupted)
+
 # print(bitarray(hamming_original(corrected, dictionary)
 #                ).tobytes().decode(ENCODING_FORMAT))
-# corrected = list(corrected)
-# for index in dictionary:
-#     corrected[index] = dictionary[index]
-# print(''.join(corrected))
-# print(get01("aa"))
+# # corrected = list(corrected)
+# # for index in dictionary:
+# #     corrected[index] = dictionary[index]
+# # print(''.join(corrected))
+# # print(get01("aa"))
